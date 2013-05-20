7024bdee9ca5a028601e0a3cf9c4e0231ff87460
Arif Datoo <arif@datoos.com>
2013-05-20 08:24:32 +0100

    Inclusion of Webjars, Bootstrap and AngularJS

--- project/Build.scala
+++ project/Build.scala
@@ -8,14 +8,12 @@ object ApplicationBuild extends Build {
   val appVersion      = "1.0-SNAPSHOT"
 
   val appDependencies = Seq(
-    // Add your project dependencies here,
-    jdbc,
-    anorm
+    "org.webjars"   % "angularjs"     % "1.1.4" ,
+    "org.webjars"   % "bootstrap"     % "2.3.1-1" ,
+    "org.webjars"   % "webjars-play"  % "2.1.0-1" 
   )
 
-
   val main = play.Project(appName, appVersion, appDependencies).settings(
-    // Add your own project settings here      
   )
 
 }
