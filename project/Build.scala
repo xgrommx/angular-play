915977fc3d1905b7c147de0e97f1e0574401d545
Arif Datoo <arif@datoos.com>
2013-06-01 09:05:47 +0100

    WebJar Dependencies - Version Upgrades

--- project/Build.scala
+++ project/Build.scala
@@ -8,9 +8,9 @@ object ApplicationBuild extends Build {
   val appVersion      = "1.0-SNAPSHOT"
 
   val appDependencies = Seq(
-    "org.webjars"   % "angularjs"     % "1.1.4" ,
-    "org.webjars"   % "bootstrap"     % "2.3.1-1" ,
-    "org.webjars"   % "webjars-play"  % "2.1.0-1" 
+    "org.webjars"   % "angularjs"     % "1.1.5-1"	,
+    "org.webjars"   % "bootstrap"     % "2.3.2"		,
+    "org.webjars"   % "webjars-play"  % "2.1.0-2" 
   )
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
